40bc48b07075d16c5ad7903c15b1f33b577678a2
Austin Lane Hearns Howard <austin.lane.howard@gmail.com>
2021-01-29 23:14:44 -0700

    Update roguelike_bot.py

--- roguelike_bot.py
+++ roguelike_bot.py
@@ -1,5 +1,6 @@
 import discord
 import level_generation
+import numpy as np
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
@@ -17,38 +18,46 @@ def blocks(infile, bufsize=1024):
 
 @client.event
 async def on_message(input):
+#
+#    Just a basic test to see if the Bot is up and running
+#
     if input.content == "test":
         await input.channel.send("The test is working.")
+#
+#    Creating the map and spitting it out on Discord
+#
+    if input.content == "create map":
+        gen = level_generation.Generator()
+        gen.gen_level()
+        gen.gen_tiles_level()
+        #with open('level.txt', 'r') as f:
+        #    for c in blocks(f, 64):
+        #        map_row = str(c)
+        #        await input.channel.send("`{}`".format(map_row))
+        await input.channel.send('Map Created!')
     await client.process_commands(input)
+#
+#    Test reading the txt file
+#
+    if input.content == "read text":
+        f = open("Start_Screen.txt", "r")
+        await input.channel.send("```" + f.read() + "```")
+
+#
+#    Test reading the map txt file
+#
+    if input.content == "read map":
+        f = open("Screensize.txt", "r")
+        await input.channel.send("```" + f.read() + "```")
+
 
 @client.event
 async def on_ready():
     print('Bot is ready.')
 
 @client.command()
-async def ping(ctx):
-    await ctx.send('Pong!')
-
-@client.command()
 async def clear(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
-
-@client.command()
-async def create_map(ctx):
-    gen = level_generation.Generator()
-    gen.gen_level()
-    gen.gen_tiles_level()
-    i = 1
-    with open('level.txt', 'r') as f:
-        for c in blocks(f, 64):
-            row_value = "Row " + str(i) + " complete!"
-            print(row_value)
-            map_row = str(c)
-            await ctx.channel.send("`{}`".format(map_row))
-            i += 1
-
-    await ctx.send('Map Created!')
-
 
 text_file = open("Info.txt", "r")
 client_secret = text_file.read(59)
