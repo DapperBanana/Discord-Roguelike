66f5f5ea1dbe15a0ed06614a9d645ead4575ed8d
Austin Lane Hearns Howard <austin.lane.howard@gmail.com>
2022-01-20 15:44:55 -0700

    Enemy spawn v2

--- map_creator.py
+++ map_creator.py
@@ -105,6 +105,10 @@ async def generate_map(file_name):
         "s",
         "p"
     }
+    randchoice = {
+        -1,
+        1
+    }
     level = 1
     amount_of_enemies = level * random.randint(1,5)
     for x in range(amount_of_enemies):
@@ -112,15 +116,15 @@ async def generate_map(file_name):
         enemy_y = random.randint(0,room_height-1)
         enemy_x = random.randint(0,room_width-1)
         if abs(enemy_y - player_y) <= 3 and abs(enemy_x - player_x) <= 3:
-            val = random.choice(-1, 1)
+            val = random.choice(randchoice)
             enemy_x += val * 3
             enemy_y += val * 3
         while grid_array[enemy_y][enemy_x] != " ":
-            val = random.choice(-1, 1)
+            val = random.choice(randchoice)
             enemy_x += val
             enemy_y += val
             if abs(enemy_y - player_y) <= 3 and abs(enemy_x - player_x) <= 3:
-                val = random.choice(-1, 1)
+                val = random.choice(randchoice)
                 enemy_x += val * 3
                 enemy_y += val * 3
         grid_array[enemy_y][enemy_x] = type_of_enemy
