3baa1a9067d6fd792626c66bef4c7c038753b8df
Austin Lane Hearns Howard <austin.lane.howard@gmail.com>
2022-02-06 20:29:16 -0700

    Update game_screen.py

--- game_screen.py
+++ game_screen.py
@@ -289,5 +289,6 @@ async def resolve_battle_screen(raw_input):
     numpy.savetxt("./player_files/current_view.txt", game_screen, fmt='%s', delimiter='')
     f = open("./player_files/current_view.txt", 'r')
     await raw_input.channel.send("```" + f.read() + "```")
+    #test output
 
     return 
