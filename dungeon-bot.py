ab0236108f451a37ef3c6324d44deaa84314013c
Austin L. Howard <austin.lane.howard@gmail.com>
2024-01-21 13:18:29 -0700

    Update dungeon-bot.py

--- dungeon-bot.py
+++ dungeon-bot.py
@@ -26,7 +26,8 @@ async def on_message(message):
         if str(message.content).lower() == "join music":  # Replace with your desired command prefix and command
             # Check if the bot has the necessary permissions to connect to a voice channel
             await message.channel.send(str(message.content).lower())
-            if message.guild.me.guild_permissions.connect and message.guild.me.guild_permissions.speak:
+            # Check if the bot has the necessary permissions to connect to a voice channel
+            if client.guild.me.guild_permissions.connect and client.guild.me.guild_permissions.speak:
                 # Get the voice channel by name
                 voice_channel = discord.utils.get(message.guild.voice_channels, name="the-catacombs-music")
     
